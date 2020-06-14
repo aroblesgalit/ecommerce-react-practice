496616dd0c20407be33f1316868d7ae776b50b87
Alvin Galit <aroblesgalit@gmail.com>
2020-06-14 17:22:41 -0500

    Created a js file to hold products data

---
+++ client/src/data.js
@@ -0,0 +1,31 @@
+export default {
+    products: [
+        {
+            name: "Slim Shirt",
+            category: "Shirts",
+            image: "/images/d1.jpg",
+            price: 60,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            name: "Fit Shirt",
+            category: "Shirts",
+            image: "/images/d2.jpg",
+            price: 65,
+            brand: "Nike",
+            rating: 4.7,
+            numReviews: 15
+        },
+        {
+            name: "Best Pants",
+            category: "Pants",
+            image: "/images/d3.jpg",
+            price: 70,
+            brand: "Nike",
+            rating: 4.6,
+            numReviews: 12
+        }
+    ]
+}
