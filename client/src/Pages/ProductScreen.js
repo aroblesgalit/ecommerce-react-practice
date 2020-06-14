1901c1bf0833458373d6583e4e57ffdd5904a015
Alvin Galit <aroblesgalit@gmail.com>
2020-06-14 18:00:18 -0500

    Styled product page

--- client/src/Pages/ProductScreen.js
+++ client/src/Pages/ProductScreen.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import data from "../data";
 
 function ProductScreen(props) {
@@ -8,7 +9,54 @@ function ProductScreen(props) {
 
     return (
         <div>
-            <h1>{product.name}</h1>
+            <div className="back-to-result">
+                <Link to="/">Back to result</Link>
+            </div>
+            <div className="details">
+                <div className="details-image">
+                    <img src={product.image} alt={product.name} />
+                </div>
+                <div className="details-info">
+                    <ul>
+                        <li>
+                            <h4>{product.name}</h4>
+                        </li>
+                        <li>
+                            {product.rating} Stars ({product.numReviews} Reviews)
+                        </li>
+                        <li>
+                            Price: <b>${product.price}</b>
+                        </li>
+                        <li>
+                            Description:
+                            <div>
+                                {product.description}
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div className="details-action">
+                    <ul>
+                        <li>
+                            Price: {product.price}
+                        </li>
+                        <li>
+                            Status: {product.status}
+                        </li>
+                        <li>
+                            Qty: <select>
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                            </select>
+                        </li>
+                        <li>
+                            <button className="button primary">Add to Cart</button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 }
