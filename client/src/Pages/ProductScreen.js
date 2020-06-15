a0d5cd769e6e09f424c6e0cc7d2141a41c5620d3
Alvin Galit <aroblesgalit@gmail.com>
2020-06-14 22:03:03 -0500

    Added a handle event on add to cart button

--- client/src/Pages/ProductScreen.js
+++ client/src/Pages/ProductScreen.js
@@ -14,6 +14,10 @@ function ProductScreen(props) {
         dispatch(detailsProduct(props.match.params.id));
     }, []);
 
+    const handleAddToCart = () => {
+        props.history.push("/cart/" + props.match.params.id + "?qty=" + qty);
+    }
+
     return (
         <div>
             <div className="back-to-result">
@@ -64,7 +68,7 @@ function ProductScreen(props) {
                                             </select>
                                         </li>
                                         <li>
-                                            <button className="button primary">Add to Cart</button>
+                                            <button onClick={handleAddToCart} className="button primary">Add to Cart</button>
                                         </li>
                                     </ul>
                                 </div>
