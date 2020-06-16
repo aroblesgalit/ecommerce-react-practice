dc97e16a1e4808d6e6e8a9e90909cdf46696609d
Alvin Galit <aroblesgalit@gmail.com>
2020-06-15 22:12:55 -0500

    Added logic for updating data on cart screen

--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 const addToCart = (productId, qty) => async (dispatch) => {
     try {
-        const { data } = await axios.get("/api/products/" + productId);
+        const { data } = await axios.get("/api/product/" + productId);
         dispatch({ type: CART_ADD_ITEM, payload: {
             product: data._id,
             name: data.name,
@@ -17,4 +17,8 @@ const addToCart = (productId, qty) => async (dispatch) => {
     }
 }
 
-export { addToCart };
+const removeFromCart = (productId) => (dispatch) => {
+    dispatch({ type: CART_REMOVE_ITEM, payload: productId });
+}
+
+export { addToCart, removeFromCart };
