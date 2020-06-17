b5f3ecc2a823bc8fb435f44b08d6c91b67e0e106
Alvin Galit <aroblesgalit@gmail.com>
2020-06-16 21:03:43 -0500

    Created user model

---
+++ server/models/userModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+});
+
+const userModel = mongoose.model("User", userSchema);
+
+export default userModel;
