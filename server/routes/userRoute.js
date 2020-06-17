c97a5bcea7bde804e9e23bb804947dc3f34b066a
Alvin Galit <aroblesgalit@gmail.com>
2020-06-16 21:08:04 -0500

    Created user route

---
+++ server/routes/userRoute.js
@@ -0,0 +1,22 @@
+import express from "express";
+import User from "../models/userModel";
+
+const router = express.Router();
+
+router.get("/api/users/createadmin", async (req, res) => {
+    try {
+        const user = new User({
+            name: "Alvin",
+            email: "alvin@gmail.com",
+            password: "password",
+            isAdmin: true
+        });
+    
+        const newUser = await user.save();
+        res.send(newUser);
+    } catch (err) {
+        res.send({ msg: err.message });
+    }
+});
+
+export default router;
